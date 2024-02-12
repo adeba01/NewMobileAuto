714cf2907cf28d3a053d92343c639e87d5341704
adeba01 <eturastinze@gmail.com>
2024-02-12 11:07:31 +0200

    created RunnerTest class

---
+++ src/test/java/com/mobframework/Runners/RunnerTest.java
@@ -0,0 +1,15 @@
+package com.mobframework.Runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = { "src/test/resources/features" }, 
+                glue = { "com.mobframework.stepdefinitions","com.mobframework.pages", "com.mobframework.hooks","com.mobframework.utils" }, 
+                plugin = { "pretty", "html:target/report/html-report.html","json:target/report/json-report.json" })
+
+public class RunnerTest {
+
+}
